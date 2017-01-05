0a6cdbb4599a121a4ff0c0c4abe3456dda399fc8
Ben Lambeth <lambethben@gmail.com>
2017-01-04 19:21:38 -0600

    Lot's of changes

--- desktop.sh
+++ desktop.sh
@@ -1,5 +1,5 @@
-# xsetroot -cursor_name left_ptr&
-# setxkbmap -layout dvorak
+xsetroot -cursor_name left_ptr&
+setxkbmap -layout dvorak
 # xmodmap /home/ben/config/dvorak
 xset r rate 200 50
 
@@ -15,6 +15,8 @@ xset r rate 200 50
 # xrandr --output DP-2 --scale 1x1 --mode 3840x2160 --pos 0x0
 # xrandr --output DP-2 --primary --left-of HDMI-0
 
-# [[ -f ~/config/rofi.conf ]] && xrdb -merge -I$HOME ~/config/rofi.conf
+[[ -f ~/config/rofi.conf ]] && xrdb -merge -I$HOME ~/config/rofi.conf
 
-# xrandr --output DP-4 --primary --auto --output DP-2 --auto --right-of DP-4
+xrandr --output DP-4 --primary --auto --output DP-2 --auto --right-of DP-4
+
+emacs --daemon
