1fbb016c248d34cd32d7318a8f24e3429e9cb33a
Ben Lambeth <lambethben@gmail.com>
2017-02-07 20:44:31 -0600

    Removed e alias

--- prezto/modules/utility/init.zsh
+++ prezto/modules/utility/init.zsh
@@ -49,7 +49,6 @@ alias sftp='noglob sftp'
 alias _='sudo'
 alias b='${(z)BROWSER}'
 alias cp="${aliases[cp]:-cp} -i"
-alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias ln="${aliases[ln]:-ln} -i"
 alias mkdir="${aliases[mkdir]:-mkdir} -p"
 alias mv="${aliases[mv]:-mv} -i"
