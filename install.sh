9a5e979ced35d653728c0af1e0380967d8c9470c
Ben Lambeth <lambethben@gmail.com>
2018-03-19 19:05:46 -0500

    Using new system

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+setopt EXTENDED_GLOB
 
 CLONE_DIR=$PWD
 
 cd $HOME
 
-for f in $CLONE_DIR/home/* $CLONE_DIR/home/.[^.]*; do
-    echo "ln -s $(basename $f) $f"
+for f in $CLONE_DIR/home/.[^.]*; do
+    LINK=$HOME/${f:t}
+    [[ -a $LINK ]] && rm -rf $LINK
+    ln -s $f $HOME/${f:t}
+done
+
+for f in $CLONE_DIR/home/.zprezto/runcoms/^README.md(.N); do
+    LINK=$HOME/.${f:t}
+    [[ -a $LINK ]] && rm -rf $LINK
+    ln -s $f $HOME/.${f:t}
 done
