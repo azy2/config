b95ed9dbe7e8a37eaaec31eb8cc2a9058e8ba65f
Ben Lambeth <lambethben@gmail.com>
2018-03-19 21:33:21 -0500

    Added kde and .config files

---
+++ home/.local/share/konsole/Profile 1.profile
@@ -0,0 +1,18 @@
+[Appearance]
+ColorScheme=Breeze
+Font=DejaVu Sans Mono,11,-1,0,50,0,0,0,0,0,Book
+
+[Cursor Options]
+CursorShape=1
+
+[General]
+Command=/bin/zsh
+Name=Profile 1
+Parent=FALLBACK/
+
+[Scrolling]
+HistoryMode=2
+ScrollBarPosition=2
+
+[Terminal Features]
+BlinkingCursorEnabled=true
--- install.sh
+++ install.sh
@@ -2,18 +2,21 @@
 
 setopt EXTENDED_GLOB
 
-CLONE_DIR=$PWD
+DOTFILE_DIR=$PWD/home
 
 cd $HOME
 
-for f in $CLONE_DIR/home/.[^.]*; do
-    LINK=$HOME/${f:t}
-    [[ -a $LINK ]] && rm -rf $LINK
-    ln -s $f $HOME/${f:t}
+DIRS=('.emacs.d' '.doom.d' '.zprezto' '.local/share/konsole' '.local/share/dolphin' '.config')
+
+for f in $DIRS; do
+    LINK=$HOME/$f
+    TARGET=$DOTFILE_DIR/$f
+    [[ -h $LINK ]] && rm $LINK
+    ln -s $TARGET $LINK
 done
 
 for f in $CLONE_DIR/home/.zprezto/runcoms/^README.md(.N); do
-    LINK=$HOME/.${f:t}
-    [[ -a $LINK ]] && rm -rf $LINK
+    LINK=$HOME/$f
+    [[ -h $LINK ]] && rm -rf $LINK
     ln -s $f $HOME/.${f:t}
 done
