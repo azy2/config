e550e793f661838d8c47912fa1f122c032bbd12d
Ben Lambeth <lambethben@gmail.com>
2018-03-27 13:43:42 -0500

    ovs environment

--- home/school/softeng/Overseer/shell.nix
+++ home/school/softeng/Overseer/shell.nix
@@ -7,17 +7,22 @@ stdenv.mkDerivation rec {
     python36Full
     python36Packages.pip
     python36Packages.virtualenv
-    mysql57
     gcc7
     libffi
     openssl
   ];
   shellHook = ''
+    sudo service mysql start
+    mysql -u root -ppass123 -e "DROP DATABASE IF EXISTS ovs; CREATE DATABASE ovs;"
     export PATH="$HOME/.pyenv/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
     pyenv activate ovs
     export FLASK_APP=main.py
     export FLASK_DEBUG=True
+    pip install -r requirements.txt
+    cd database
+    alembic upgrade head
+    cd ..
   '';
 }
